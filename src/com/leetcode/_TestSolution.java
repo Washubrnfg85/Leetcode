ea347f177cc090459b62262dfa8aec93959409f6
ArchPJ <washubrnfg85@gmail.com>
2023-06-19 23:05:01 +0300

    Binaty Search solution added

--- src/com/leetcode/_TestSolution.java
+++ src/com/leetcode/_TestSolution.java
@@ -2,7 +2,8 @@ package com.leetcode;
 
 public class _TestSolution {
     public static void main(String[] args) {
-        Shuffle_String ss = new Shuffle_String();
-        System.out.println(ss.restoreString("codeleet", new int[]{4,5,6,7,0,2,1,3}));
+        Binary_Search bs = new Binary_Search();
+        int[] nums = new int[]{-1,0,3,5,9,12};
+        System.out.println(bs.search(nums, 2));
     }
 }
