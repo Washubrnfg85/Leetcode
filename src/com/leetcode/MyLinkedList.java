c6ea9d6eb176eb50916dd5c00471e55fc758d732
ArchPJ <washubrnfg85@gmail.com>
2023-06-26 00:48:45 +0300

    adding MyLinkedList progress

--- src/com/leetcode/MyLinkedList.java
+++ src/com/leetcode/MyLinkedList.java
@@ -1,37 +1,154 @@
 package com.leetcode;
 
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
+
 public class MyLinkedList {
-    /**
-     * Your MyLinkedList object will be instantiated and called as such:
-     * MyLinkedList obj = new MyLinkedList();
-     * int param_1 = obj.get(index);
-     * obj.addAtHead(val);
-     * obj.addAtTail(val);
-     * obj.addAtIndex(index,val);
-     * obj.deleteAtIndex(index);
-     */
+    private Node root;
+
+    public class Node {
+        private int value;
+        private Node prev;
+        private Node next;
+
+        public Node (int val) {
+            this.value = val;
+        }
+    }
 
     public MyLinkedList() {
-
+        this.root = null;
     }
 
     public int get(int index) {
-
+        if (root == null) {
+            return -1;
+        } else {
+            if (index == 0)
+                return root.value;
+            int count = 0;
+            Node current = root;
+            while (count < index) {
+                if (current.next == null) return -1;
+                current = current.next;
+                if (current == null) return -1;
+                count++;
+            }
+            return current.value;
+        }
     }
 
     public void addAtHead(int val) {
-
+        Node first = new Node(val);
+        if (root == null) {
+            root = first;
+            return;
+        } else {
+            Node second = root;
+            root = first;
+            second.prev = first;
+            first.next = second;
+        }
     }
 
     public void addAtTail(int val) {
-
+        Node last = new Node(val);
+        if (root == null) {
+            root = last;
+            return;
+        } else {
+            Node current = root;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = last;
+            last.prev = current;
+        }
     }
 
     public void addAtIndex(int index, int val) {
-
+        Node node = new Node(val);
+        if (root == null) {
+            if (index == 0) {
+                root = node;
+                return;
+            }
+            return;
+        } else {
+            if (index == 0) {
+                addAtHead(val);
+            } else if (index > getSize() - 1) {
+                return;
+            } else {
+                int count = 1;
+                Node current = root;
+                while (count < index) {
+                    count++;
+                    current = current.next;
+                }
+                Node next = current.next;
+                current.next = node;
+                node.prev = current;
+                node.next = next;
+                next.prev = node;
+            }
+        }
     }
 
     public void deleteAtIndex(int index) {
+        if (root == null) {
+            return;
+        } else {
+            if (index == 0) {
+                root = root.next;
+                root.prev = null;
+            } else if (index > getSize() - 1) {
+                return;
+            } else {
+                int count = 0;
+                Node current = root;
+                while (count != index) {
+                    count++;
+                    current = current.next;
+                }
+                Node toDelete = current;
+                Node previous = current.prev;
+                Node next = current.next;
+                previous.next = next;
+                next.prev = previous;
+                toDelete = null;
 
+            }
+        }
+    }
+
+    public int getSize() {
+        if (root == null) return 0;
+        int count = 1;
+        Node current = root;
+        while (current.next != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        if (root != null && root.next == null) return "" + root.value;
+        StringBuilder sb = new StringBuilder();
+        Node current = root;
+        while (current != null) {
+            sb.append(current.value);
+            current = current.next;
+        }
+        return sb.toString();
     }
 }
--- src/com/leetcode/_TestSolution.java
+++ src/com/leetcode/_TestSolution.java
@@ -2,7 +2,10 @@ package com.leetcode;
 
 public class _TestSolution {
     public static void main(String[] args) {
-        Intersection_of_Two_Arrays a = new Intersection_of_Two_Arrays();
-        System.out.println(a.intersection(new int[]{1,2,2,1}, new int[]{2,2}));
+        MyLinkedList list = new MyLinkedList();
+
+        list.addAtHead(4);
+        System.out.println(list.get(1));
+
     }
 }
