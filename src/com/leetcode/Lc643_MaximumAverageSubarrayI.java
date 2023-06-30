95162b84ccd06562db22fee5f950b4ac8c3c7f24
Aleksandr Sergeev <s.w85@yandex.ru>
2023-06-30 18:56:22 +0300

    30.06.2023 18:55 Form Work  solving MasAvSubArray

--- src/com/leetcode/Lc643_MaximumAverageSubarrayI.java
+++ src/com/leetcode/Lc643_MaximumAverageSubarrayI.java
@@ -6,19 +6,24 @@ public class Lc643_MaximumAverageSubarrayI {
     public double findMaxAverage(int[] nums, int k) {
         if (k < 1 | k > nums.length) throw new IllegalArgumentException();
         if (nums.length == 1 & k == 1)  return ((double) nums[0]) / k;
+        if (nums.length == k) {
+            double sum = 0.0;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
+            return sum / k;
+        }
 
         double maxAverageValue = 0.0;
-        for (int i = 0; i < nums.length - k; i++) {
+        for (int i = 0; i <= nums.length - k; i++) {
             double sum = 0;
             for (int j = i; j < i + k; j++) {
                 sum += nums[j];
             }
-            System.out.println(sum);
             if (maxAverageValue < sum / k) {
                 maxAverageValue = sum / k;
             }
         }
-
         return maxAverageValue;
     }
 }
