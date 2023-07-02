9038e341b4535116be5a0ce1c11d6edf2ab686f4
ArchPJ <washubrnfg85@gmail.com>
2023-07-02 13:29:52 +0300

    delete comments and sout's from CompletingWords

--- src/com/leetcode/Lc748_ShortestCompletingWord.java
+++ src/com/leetcode/Lc748_ShortestCompletingWord.java
@@ -5,15 +5,10 @@ import java.util.*;
 
 public class Lc748_ShortestCompletingWord {
     public String shortestCompletingWord(String licensePlate, String[] words) {
-        //deleting numbers, spaces and casting to lowerCase
         String plateIgnoreNumbersAndSpaces = licensePlate.replaceAll("[\\d\" \"]", "").toLowerCase();
-        System.out.println(plateIgnoreNumbersAndSpaces);
-        //making an array of letters from plate
         String[] plateLetters = plateIgnoreNumbersAndSpaces.split("");
-        //declaring a map to store letter as Key and number of them as Value
         Map<String, Integer> countPlateLetters = new HashMap<>();
 
-        //filling the map
         for (String letter : plateLetters) {
             if (countPlateLetters.containsKey(letter)) {
                 Integer value = countPlateLetters.get(letter);
@@ -23,12 +18,9 @@ public class Lc748_ShortestCompletingWord {
             }
         }
 
-        //declaring a list to store completing words
         List<String> completingWords = new ArrayList<>();
         for (String word : words) {
-            //making an array of letters from each word
             String[] wordLetters = word.split("");
-            //declaring a map to store word's letters and there quantity
             Map<String, Integer> countWordLetters = new HashMap<>();
             //filling the map
             for (String letter : wordLetters) {
@@ -40,13 +32,10 @@ public class Lc748_ShortestCompletingWord {
                 }
             }
 
-            //checking the accordance
             if (checkForAccordance(countPlateLetters, countWordLetters))
                 completingWords.add(word);
         }
-        System.out.println(completingWords);
 
-        //getting the final answer
         if (completingWords.size() < 2) {
             return completingWords.get(0);
         } else {
@@ -54,7 +43,6 @@ public class Lc748_ShortestCompletingWord {
         }
     }
 
-    //method to check if word (candidate) contains all letters of plate in same of more quantity
     private boolean checkForAccordance(Map<String, Integer> plate, Map<String, Integer> candidate) {
         int count = 0;
         for(Map.Entry<String, Integer> entry : plate.entrySet()) {
@@ -65,7 +53,6 @@ public class Lc748_ShortestCompletingWord {
         return  count == plate.size();
     }
 
-    //method to get the shortest word from specified list
     private String getTheShortestWordFromList(List<String> list) {
         int minWordLength = 0;
         String shortestWord = "";
